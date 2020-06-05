68353552c4947e6717776d7602431273daa17124
bsubbaraman <bms12014@mymail.pomona.edu>
2020-06-05 15:58:39 -0700

    initial commit

---
+++ Source/LaInvencion/LaInvencionGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "LaInvencionGameMode.h"
+#include "LaInvencionCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ALaInvencionGameMode::ALaInvencionGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
